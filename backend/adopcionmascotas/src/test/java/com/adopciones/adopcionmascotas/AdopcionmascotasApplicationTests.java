29c42f23fddfdab06dcccac384e692928bfba9ab
Matias Ezequiel Mamani <matias.ezequiel.1177@gmail.com>
2025-05-21 13:49:12 -0300

    Primera versión del backend: estructura base del proyecto Spring Boot

---
+++ backend/adopcionmascotas/src/test/java/com/adopciones/adopcionmascotas/AdopcionmascotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adopciones.adopcionmascotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopcionmascotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
