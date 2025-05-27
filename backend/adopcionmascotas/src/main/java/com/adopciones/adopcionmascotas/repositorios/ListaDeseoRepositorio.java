9d03650cc41419c00000b3e72b5dc1c0e86dc866
yoel <aquispec@fi.uba.ar>
2025-05-27 18:20:40 -0300

    fix(entidad listaDeseo): se agrega controlador y repositorio de la entidad ListaDeseo y ListaDeseoId

---
+++ backend/adopcionmascotas/src/main/java/com/adopciones/adopcionmascotas/repositorios/ListaDeseoRepositorio.java
@@ -0,0 +1,16 @@
+package com.adopciones.adopcionmascotas.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.adopciones.adopcionmascotas.modelos.ListaDeseo;
+import com.adopciones.adopcionmascotas.modelos.ListaDeseoId;
+import com.adopciones.adopcionmascotas.modelos.Usuario;
+import com.adopciones.adopcionmascotas.modelos.Mascota;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListaDeseoRepositorio extends JpaRepository<ListaDeseo, ListaDeseoId> {
+    List<ListaDeseo> findByUsuario(Usuario usuario);
+    Optional<ListaDeseo> findByUsuarioAndMascota(Usuario usuario, Mascota mascota);
+}
